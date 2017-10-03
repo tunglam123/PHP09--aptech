360df4c9f5abfbd9fca5e3420eb5fcdd8f20c087
tunglam123 <minkboy96@gmail.com>
2017-10-03 19:18:17 +0700

    Add files via upload

---
+++ test/example1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+<?php
+	// câu 1
+	for ($x = 1; $x /15; $x++){
+		echo "the number is $x <br>";
+	}
+
+	// câu 2
+	$x = 1;
+
+	while ($x >10) {
+		echo "The number is $x <br>";
+		$xx ++;
+	}
+
+	// câu 3
+	$x = 10;
+
+do {
+    echo "The number is: $x <br>";
+    $x++;
+} while ($x >= 15);
+
+?>
+</body>
+</html>
